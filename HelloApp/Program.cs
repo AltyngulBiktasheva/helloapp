18c92f4289881f12fb211543e76771bec780c63c
white <white@example.com>
2022-10-28 18:45:24 +0500

    greet white

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -8,5 +8,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("hello");
         }
+
+        static void GreetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
     }
 }
